3c29e57c0e2631bd533408055acd5d5131150798
Thinh-1102 <100088292+Thinh-1102@users.noreply.github.com>
2022-03-28 23:24:02 +0700

    firstcommit

---
+++ js/slick.app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 300,
+    });
+  });
